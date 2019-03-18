3a1232eda1c8fd302bf134afbd0c6bc7ce013cf8
Andrew <andrewwos88@gmail.com>
2019-03-18 21:45:28 +0700

    + add project

---
+++ WpfApp1/DataAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class DataAccount
+    {
+        public string Name, Login, Password, OtherInf;
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ WpfApp1/EncryptDecrypt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class EncryptDecrypt
+    {
+        public static byte[] Decrypt(string FILE_NAME, int KEY)
+        {
+            try
+            {
+                byte[] buf = File.ReadAllBytes(FILE_NAME);
+
+                for (int i = 0; i < buf.Length; i++)
+                {
+                    buf[i] = (byte)(buf[i] ^ KEY);
+                }
+                return buf;
+            }
+            catch (FileNotFoundException)
+            {
+                return null;
+            }
+            catch (Exception)
+            {
+                AttentionW win = new AttentionW();
+                win.InformationText.Text = "Error";
+                win.ShowDialog();
+            }
+            return null;
+        }
+
+        public static byte[] Encrypt(MemoryStream stream, int KEY)
+        {
+            byte[] buf = stream.ToArray();
+
+            for (int i = 0; i < buf.Length; i++)
+            {
+                buf[i] = (byte)(buf[i] ^ KEY);
+            }
+            return buf;
+        }
+
+        public static byte[] EncryptPass(string pass, int KEY)
+        {
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter bw = new BinaryWriter(stream))
+            {
+                bw.Write(pass);
+            }
+            return Encrypt(stream, KEY);
+        }
+    }
+}
