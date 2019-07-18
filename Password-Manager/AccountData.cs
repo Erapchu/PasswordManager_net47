b3a6ee59317239c07a8454a8722027761fc87927
Andrew <andrewwos88@gmail.com>
2019-07-19 00:44:11 +0700

    * небольшие правки

--- Password-Manager/AccountData.cs
+++ Password-Manager/AccountData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,7 +21,7 @@ namespace Password_Manager
             set
             {
                 _name = value;
-                OnPropertyChanged("Name");
+                OnPropertyChanged();
             }
         }
 
@@ -34,7 +35,7 @@ namespace Password_Manager
             set
             {
                 _login = value;
-                OnPropertyChanged("Login");
+                OnPropertyChanged();
             }
         }
 
@@ -48,7 +49,7 @@ namespace Password_Manager
             set
             {
                 _password = value;
-                OnPropertyChanged("Password");
+                OnPropertyChanged();
             }
         }
 
@@ -62,7 +63,7 @@ namespace Password_Manager
             set
             {
                 _other = value;
-                OnPropertyChanged("Other");
+                OnPropertyChanged();
             }
         }
 
@@ -89,9 +90,9 @@ namespace Password_Manager
 
         #region MVVM Pattern
         public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged(string property_name)
+        void OnPropertyChanged([CallerMemberName]string propertyName = "")
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
     }
--- Password-Manager/MainViewModel.cs
+++ Password-Manager/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,13 +17,26 @@ namespace Password_Manager
             AccountDatas = new ObservableCollection<AccountData>(AccountData.ReadFile());
         }
 
+        private AccountData _selectedAccount;
+        public AccountData SelectedAccount
+        {
+            get
+            {
+                return _selectedAccount;
+            }
+            set
+            {
+                _selectedAccount = value;
+                OnPropertyChanged();
+            }
+        }
 
 
         #region MVVM Pattern
         public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged(string property_name)
+        void OnPropertyChanged([CallerMemberName]string propertyName = "")
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
     }
