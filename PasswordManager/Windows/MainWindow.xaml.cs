4cc81ca4b3af30bfe4107871cbfc37cf27d8d318
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-08-15 16:16:39 +0700

    Added ChromeWindow

--- PasswordManager/Windows/MainWindow.xaml.cs
+++ PasswordManager/Windows/MainWindow.xaml.cs
@@ -32,11 +32,53 @@ namespace PasswordManager.Windows
             var windowHandle = wih.EnsureHandle();
             viewModel = _lifetimeScope.Resolve<MainViewModel>(new NamedParameter("windowHandle", windowHandle));
             DataContext = viewModel;
+            StateChanged += MainWindow_StateChanged;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void MainWindow_StateChanged(object sender, EventArgs e)
         {
-            //TODO Start reading file
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(8);
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        // Can execute
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        // Maximize
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        // Restore
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
         }
 
         private void Window_Closed(object sender, EventArgs e)
